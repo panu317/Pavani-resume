27748ad61273cf2b12a5cd786337a4397518315b
Pavani <pavanipanu95@gmail.com>
2023-05-08 01:17:38 -0500

    my Resume
    Resume

---
+++ main.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+});
+
+
+const localApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApi = 'https://resumefunction7.azurewebsites.net/api/Resumefunc?code=NlEgctB4qVpCQlxwgiKq55S1taXccbIIhlp5/J2uhHH5j7ZU0rzaqQ=='; 
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi)
+    .then(response => {
+        return response.json()
+    })
+    .then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
+        console.log(error);
+      });
+    return count;
+}
